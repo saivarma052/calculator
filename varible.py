34c0992a259bcf1e287591b436711ba767567312
EC2 Default User <ec2-user@ip-172-31-16-184.ec2.internal>
2024-07-01 07:17:34 +0000

    adding varibles

---
+++ varible.py
@@ -0,0 +1,26 @@
+int_decl_x = 20
+float_decl_f = 2.5
+string_decl_s = "sai"
+bol_decl_is_boolt = True
+bol_decl_is_boolf = False
+List_decl_nums = [1, 2, 3]
+tuple_decl_tup = (10, 20)
+dict_decl_dictionary = {"name": "Sai", "age": 25}
+unique_numbers = {1, 2, 3}
+print(f"int_decl_x is {type(int_decl_x)}")
+print(f"float_decl_f is {type(float_decl_f)}")
+print(f"string_decl_s is {type(string_decl_s)}")
+print(f"bol_decl_boolt is {type(bol_decl_is_boolt)}")
+print(f"bol_decl_boolf is {type(bol_decl_is_boolf)}")
+print(f"List_decl_nums is {type(List_decl_nums)}")
+print(f"tuple_decl_tup is {type(tuple_decl_tup)}")
+print(f"dict_decl_dictionary is {type(dict_decl_dictionary)}")
+print(f"unique_numbers is {type(unique_numbers)}")
+
+#int_decl_x = .5
+#int_decl_x = .5
+#int_decl_x = .5
+#int_decl_x = .5
+
+
+
